914602dc4c51eef64af1c9eb71b28a9574cfc557
caoyang <161484041@qq.com>
2025-10-21 21:01:17 +0800

    苍穹外卖

---
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -0,0 +1,22 @@
+package com.sky.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class EmployeeDTO implements Serializable {
+
+    private long id;
+
+    private String username;
+
+    private String name;
+
+    private String phone;
+
+    private String sex;
+
+    private String idNumber;
+
+}
